d5f9c1205f7e06378a12bf6f5a8f28e4b06f8fc7
Kirillbiliashov <73443541+Nutulut210@users.noreply.github.com>
2022-01-01 22:09:22 +0200

    implementation of task 4 was refined

--- Exercises/4-methods.js
+++ Exercises/4-methods.js
@@ -1,26 +1,26 @@
 'use strict';
 
 const methods = iface => {
+  const res = [];
   const entries = Object.entries(iface);
-  entries.map(arr => {
-    const value = arr[1];
-    if (typeof value === 'function') {
-       arr[1] =  value.length
-    }
-  })
-  return entries
-  }
+  entries.forEach(([key, val]) => {
+    if (typeof val === 'function') res.push([key, val.length]);
+  });
+  return res;
+}
 
-  const iface = {
-    m1: x => [x],
-    m2: function (x, y) {
-      return [x, y];
-    },
-    m3(x, y, z) {
-      return [x, y, z];
-    }
-  }
-  
-  const iterated = methods(iface);
+const iface = {
+  m1: x => [x],
+  m2: function (x, y) {
+    return [x, y];
+  },
+  m3(x, y, z) {
+    return [x, y, z];
+  },
+}
+
+const iterated = methods(iface);
+
+console.dir({ iterated });
 
 module.exports = { methods };
